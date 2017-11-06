53fc73e5b0577b1574f3b20d9f3e6ee2ab735244
Qingyue99 <31904741+Qingyue99@users.noreply.github.com>
2017-11-06 12:50:45 -0500

    process 5

--- PS01/index.js
+++ PS01/index.js
@@ -1,14 +1,16 @@
 var width = document.getElementById('svg').clientWidth;
 var height = document.getElementById('svg').clientHeight;
+var centered;
 
 var marginLeft = 0;
 var marginTop = 10;
 
-var svg = d3.select('svg')
-    .append('g')
-    .attr('transform', 'translate(' + marginLeft + ',' + marginTop + ')')
-    .attr("class", "graph-svg-component");
+var svg = d3.select('svg');
 
+
+var g = svg.append('g')
+    .attr('transform', 'translate(' + marginLeft + ',' + marginTop + ')')
+    .on("click", clicked);
 
 var inputValue = null;
 var distance = ["Within half Miles","Within One Miles","Within Two Miles","Within Three Miles"];
@@ -32,8 +34,10 @@ var zoomSettings = {
     zoomLevel: 5
 };
 
+
+
 function clicked(d){
-    var x;
+  /*  var x;
     var y;
     var zoomLevel;
 
@@ -54,7 +58,29 @@ function clicked(d){
     g.transition()
         .duration (zoomSettings.duration)
         .ease(zoomSettings.ease)
-        .attr('transform', 'translate(' + marginLeft + ',' + marginTop + ')');
+        .attr('transform', 'translate(' + marginLeft + ',' + marginTop + ')');*/
+    var x, y, k;
+
+    if (d && centered !== d) {
+        var centroid = path.centroid(d);
+        x = centroid[0];
+        y = centroid[1];
+        k = 4;
+        centered = d;
+    } else {
+        x = width / 2;
+        y = height / 2;
+        k = 1;
+        centered = null;
+    }
+
+    g.selectAll("path")
+        .classed("active", centered && function(d) { return d === centered; });
+
+    g.transition()
+        .duration(750)
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+        .style("stroke-width", 1.5 / k + "px");
 
 }
 
@@ -73,7 +99,8 @@ d3.json('./neighborhood_boston.json', function(dataIn){
         .attr('cursor','pointer')
         .on('mouseover', function(d){
             console.log(d.properties.NAME);
-        });
+        })
+        .on("click", clicked);
 
 
 
@@ -88,22 +115,27 @@ d3.json('./neighborhood_boston.json', function(dataIn){
         .attr('cy', function (d){
             return albersProjection([d.long, d.lat])[1];
         })
-        .attr('r', 5)
-
+        .attr('r',3.5)
         .attr('fill', '#007245')
-        //.attr('data-toggle','tooltip')
-        //.attr('title', function (d) {return d.women});;
-
+        .on('mouseover', function(d){
+            d3.select("h2").text(d.Name);
+            d3.select(this).attr("class","incident hover");
+            d3.select(this).attr("fill","#000000");
+        })
+        .on("mouseout", function(d){
+       //     d3.select("h2").text("hhh");
+       //     d3.select(this).attr("class","incident");
+            d3.select(this).attr("fill",'#007245');})
 
 });
 
 
 
 Arraylist= [
-    {long:-71.090508, lat:42.340052},
-    {long:-71.086145, lat:42.338542},
-    {long:-71.081518, lat:42.341663},
-{long:-71.076440, lat:42.348695}
+    {long:-71.090508, lat:42.340052, Name:"Gym1" },
+    {long:-71.086145, lat:42.338542, Name:"Gym2"},
+    {long:-71.081518, lat:42.341663, Name:"Gym3" },
+{long:-71.076440, lat:42.348695, Name:"Gym4" }
 ];
 
 //$('[data-toggle="tooltip"]').tooltip();
